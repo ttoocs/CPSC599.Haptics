11a3f758a25df683a068d6824bc291bbb4110452
ttoocs <ttoocs@gmail.com>
2018-04-06 21:05:29 -0600

    Add cmake-flags for quickbuilding.

--- quickSetup.sh
+++ quickSetup.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-
-BASEDIR=$(dirname "$0")
-
+BASE=$(readlink -f "$0")
+BASEDIR=$(dirname "$BASE")
+echo "BASEDIR: " $BASEDIR
 JOBS=2;
 if [ "$1" != "" ]; then
   JOBS=$1
 fi
 
+ARGS=" -DCMAKE_BUILD_TYPE=RELWITHDEBINFO "
+
 mkdir chaiBuild
 mkdir bulletBuild
 mkdir appBuild
-(cd chaiBuild && cmake $BASEDIR/../../ && make -j $JOBS )&&\
-(cd bulletBuild && cmake $BASEDIR/../../modules/BULLET/ && make -j $JOBS )&&\
-(cd appBuild && cmake $BASEDIR && make -j $JOBS )
+(cd chaiBuild && cmake $ARGS $BASEDIR/../../ && make -j $JOBS )&&\
+(cd bulletBuild && cmake $ARGS $BASEDIR/../../modules/BULLET/ && make -j $JOBS )&&\
+(cd appBuild && cmake $ARGS $BASEDIR && make -j $JOBS )
 
 echo "Done"
