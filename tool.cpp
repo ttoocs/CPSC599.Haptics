4e24943ac3a1125c61ed9c4c769460866b56c7e4
ttoocs <ttoocs@gmail.com>
2018-04-07 22:21:19 -0600

    Workspace managment, with normal-detection/avoidance/etc.

--- tool.cpp
+++ tool.cpp
@@ -9,6 +9,10 @@ chai3d::cHapticDeviceHandler* handler;
 chai3d::cGenericHapticDevicePtr hapticDevice; 
 double toolRadius = 0.006;
 
+vec3 ws_mgmt_dir;
+
+bool forceFeedbackMove = true;
+
 using namespace chai3d;
 using namespace std;
 void toolInit(){
@@ -39,7 +43,7 @@ void toolInit(){
     tool->start();
 
     // map the physical workspace of the haptic device to a larger virtual workspace.
-    tool->setWorkspaceRadius(0.8);
+    //tool->setWorkspaceRadius(0.8);
 
     // define a radius for the tool (graphical display)
     tool->setRadius(toolRadius, toolRadius);
@@ -59,7 +63,12 @@ void toolInit(){
 
     // start the haptic tool
     tool->start();
+  
+    //addForce = vec3(0,0,0);
+    ws_mgmt_dir = vec3(0,0,0);
+    
 }
+
 
 void toolHapticA(){
         // compute global reference frames for each object
@@ -71,15 +80,46 @@ void toolHapticA(){
         // compute interaction forces
         tool->computeInteractionForces();
 
+        // get pointer to next interaction point of tool
+        cHapticPoint* interactionPoint = tool->getHapticPoint(0);
 
-            // get pointer to next interaction point of tool
-            cHapticPoint* interactionPoint = tool->getHapticPoint(0);
+        //WorkSpace Mgmt:
+//        if(interactionPoint->getNumCollisionEvents() == 0)
+        {
+          //Get workspace radius of tool:
+
+          double rad = hapticDevice->getSpecifications().m_workspaceRadius;
+          rad*=.15; //Take half of it
+
+          rad = 0.025; //Say screw it and set it to this as the above seems garbage now?
+      
+          vec3 tpos = tool->getDeviceLocalPos();
+          double tlen = tpos.length();
+//          std::cout << "len:" <<  tlen << std::endl;
+          if(tlen > rad){
+            vec3 dir = cNormalize( tpos);
+            vec3 offset = dir*0.001;
+            offset *= (tlen-rad)*30;
+            ws_mgmt_dir = offset;
+            //tool->setLocalPos(tool->getLocalPos() + offset);;
+//            std::cout << "In: " << ws_mgmt_dir << std::endl;
+          }
+
+        }
 
             // check primary contact point if available
 //            /*
             for( int i =0 ; i < interactionPoint->getNumCollisionEvents(); i++)
             {
                 cCollisionEvent* collisionEvent = interactionPoint->getCollisionEvent(i);
+                
+                //Fix workspace mgmt vectors aganst the one we just hit.
+                //Subtract the projection along the normal (ie, no moment along normal)
+                if((collisionEvent->m_localNormal.x() != 0) || (collisionEvent->m_localNormal.y() != 0) || (collisionEvent->m_localNormal.z() != 0)){
+                  ws_mgmt_dir -= chai3d::cProject( ws_mgmt_dir,  collisionEvent->m_localNormal);
+                }else{
+                  std::cout << "No normal: " << collisionEvent->m_localNormal << std::endl;
+                }
 
                 // given the mesh object we may be touching, we search for its owner which
                 // could be the mesh itself or a multi-mesh object. Once the owner found, we
@@ -103,6 +143,13 @@ void toolHapticA(){
 }
 
 void toolHapticB(){
+        if(forceFeedbackMove){
+          vec3 addForce = -ws_mgmt_dir * 10000;
+          tool->addDeviceLocalForce(addForce);
+        }
+        tool->setLocalPos(tool->getLocalPos() + ws_mgmt_dir);
+
+        ws_mgmt_dir = 0;
           // send forces to haptic device
         tool->applyToDevice();
 }
