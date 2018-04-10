999539399c120c548907c354a752ae5741ae0cab
ttoocs <ttoocs@gmail.com>
2018-04-09 21:48:44 -0600

    Attempt at more speed-scaling, I don't like it.

--- sim.cpp
+++ sim.cpp
@@ -10,12 +10,16 @@ namespace sim {
   double bullet_maxIter = 2;
   double haptic_mul = 1.0/2;
 
-  bool haptic_cMin = false;
+  bool haptic_cMin = true;
   bool haptic_Clamp = true;
   
-  #define print std::cout << "Haptic_cnt set to:\t" << haptic_cnt << std::endl;
+//  #define DIV_SCALE
+  #define CNT_SCALE
 
   void scale(){
+    
+  #ifdef CNT_SCALE
+    #define print std::cout << "Haptic_cnt set to:\t" << haptic_cnt << std::endl;
     if(freqCounterHaptics.getFrequency() < 500){
       haptic_cnt ++;
       print
@@ -26,6 +30,24 @@ namespace sim {
     }
     if(haptic_cnt < 0)
       haptic_cnt = 0;
+    #undef print
+  #endif
 
+  #ifdef DIV_SCALE
+    #define print std::cout << "Haptic_mul set to:\t" << haptic_mul << std::endl;
+    if(freqCounterHaptics.getFrequency() > 500){
+      haptic_mul = pow(haptic_mul,.9);
+      print
+    }
+    if(freqCounterHaptics.getFrequency() < 900){
+      haptic_mul = pow(haptic_mul,1.1);
+      print
+    }
+    if(haptic_mul >= 1)
+      haptic_mul = 1;
+    if(haptic_mul <= 0.0001)
+      haptic_mul = 0.0001;
+
+  #endif
   }
 };
